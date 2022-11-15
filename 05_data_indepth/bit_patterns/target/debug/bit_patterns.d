0222a073c7a7ad3eb27f571129d3ac2b2598e02a
jumzzz <jummel19@gmail.com>
2022-11-14 17:25:57 -0800

    remove target

--- 05_data_indepth/bit_patterns/target/debug/bit_patterns.d
+++
@@ -1,1 +0,0 @@
-/home/jummel/project/rust/rust-action/05_data_indepth/bit_patterns/target/debug/bit_patterns: /home/jummel/project/rust/rust-action/05_data_indepth/bit_patterns/src/main.rs
--- 05_data_indepth/bit_patterns/target/debug/deps/bit_patterns-05177f25a57ac164.d
+++
@@ -1,5 +0,0 @@
-/home/jummel/project/rust/rust-action/05_data_indepth/bit_patterns/target/debug/deps/bit_patterns-05177f25a57ac164: src/main.rs
-
-/home/jummel/project/rust/rust-action/05_data_indepth/bit_patterns/target/debug/deps/bit_patterns-05177f25a57ac164.d: src/main.rs
-
-src/main.rs:
