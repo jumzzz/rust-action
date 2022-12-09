2fbf2df69063c83e8c6bcf7a725230f4be7dfc64
jumzzz <jummel19@gmail.com>
2022-12-09 13:41:41 -0800

    cpu_ria_03

---
+++ 05_data_indepth/cpu_ria_03/src/main.rs
@@ -0,0 +1,154 @@
+struct CPU {
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 4096],
+    stack: [u16; 16],
+    stack_pointer: usize,
+}
+
+
+
+#[allow(dead_code)]
+fn load_mem_v1() {
+    let mut memory: [u8; 4096] = [0; 4096];
+    let mem = &mut memory;
+
+    let add_twice = [
+        0x80, 0x14,
+        0x80, 0x14,
+        0x00, 0xEE,
+    ];
+
+    mem[0x100..0x106].copy_from_slice(&add_twice);
+
+    println!("mem = {:?}", &mem[0x100..0x106]);
+    println!("mem = {:?}", &add_twice);
+}
+
+#[allow(dead_code)]
+fn load_mem_v2() {
+    let mut memory: [u8; 4096] = [0; 4096];
+    let mem = &mut memory;
+
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
+
+    println!("mem = {:?}", &mem[0x100..0x106]);
+}
+
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+    }
+
+    fn add_xy(&mut self, x: u8, y: u8) {
+
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow) = arg1.overflowing_add(arg2);  // overflow returns as true when overflow is detected
+        self.registers[x as usize] = val;
+
+        if overflow {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+
+    fn run(&mut self) {
+
+        loop {
+            
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
+        
+            // Parses opcode to c,x,y,d
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
+
+            let nnn = opcode & 0x0FFF;
+
+            // Loop terminates when opcode=0x0000
+            println!("position_in_memory = 0x{:04X}", self.position_in_memory);
+            println!("opcode = 0x{:04X}, \tself.registers[0] = {}", opcode, self.registers[0]);
+            
+            match (c, x, y, d) {
+                (0, 0, 0, 0)        => { return; } ,
+                (0, 0, 0xE, 0xE)    => self.ret(),
+                (0x2, _, _, _)      => self.call(nnn),
+                (0x8, _, _, 0x4)    => self.add_xy(x, y),
+                _                   => todo!("opcode {:04x}", opcode),
+            }
+        }    
+    }
+
+}
+
+
+impl CPU {
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack Overflow!")
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack Underflow")
+        }
+
+        self.stack_pointer -= 1;
+        let call_addr = self.stack[self.stack_pointer];
+        self.position_in_memory = call_addr as usize;
+    }
+
+}
+
+
+
+fn main() {
+    let mut cpu = CPU {
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
+    };
+
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80; mem[0x101] = 0x14;
+    mem[0x102] = 0x80; mem[0x103] = 0x14;
+    mem[0x104] = 0x00; mem[0x105] = 0xEE;
+
+    cpu.run();
+
+    println!("cpu.registers[0] = {}", cpu.registers[0]);
+
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
+
+}
+
