154b172ce16053c2855b1ab9b407884b2672691d
jumzzz <jummel19@gmail.com>
2022-12-15 09:56:58 -0800

    yess

---
+++ 06_memory/raw_ptr/src/main.rs
@@ -0,0 +1,20 @@
+#[allow(dead_code)]
+fn simple_raw_ptr() {
+    let a: i64 = 42;
+    let a_ptr = &a as *const i64;
+    let a_addr: usize = unsafe {
+        std::mem::transmute(a_ptr)
+    };
+
+    println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
+}
+
+fn main() {
+    let ptr = 42 as *const Vec<String>;
+
+    unsafe {
+        let new_addr = ptr.offset(4);
+        println!("{:p} -> {:p}", ptr, new_addr);
+    }
+
+}
