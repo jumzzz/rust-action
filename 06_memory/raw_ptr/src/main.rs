0bc2dfbd5cb5e974bde52cd96f785fffbd892b8d
jumzzz <jummel19@gmail.com>
2022-12-15 11:13:46 -0800

    ptr

--- 06_memory/raw_ptr/src/main.rs
+++ 06_memory/raw_ptr/src/main.rs
@@ -9,12 +9,30 @@ fn simple_raw_ptr() {
     println!("a: {} ({:p}...0x{:x})", a, a_ptr, a_addr + 7);
 }
 
-fn main() {
+#[allow(dead_code)]
+fn casting_const_vec_ptr() {
+
     let ptr = 42 as *const Vec<String>;
 
     unsafe {
         let new_addr = ptr.offset(4);
         println!("{:p} -> {:p}", ptr, new_addr);
     }
+}
 
+#[allow(dead_code)]
+fn locality_str() {
+    let t0 = 8_u8;
+    let s0: &[u8] = &[b'h', b'e', b'l', b'l', b'o', b' ', b'w', b'o', b'r', b'l', b'd'];
+    let s1 = 8_u8;
+    let s2 = 8_u8;
+
+    println!("t0: {:p}", &t0);
+    println!("s0: {:p}", &s0);
+    println!("s1: {:p}", &s1);
+    println!("s2: {:p}", &s2);
+}
+
+fn main() {
+    locality_str();
 }
