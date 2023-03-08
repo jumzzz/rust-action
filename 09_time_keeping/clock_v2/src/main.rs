b85e508076e48edbea5f2e4a4a3566a17f61f2b0
jumzzz <jummel19@gmail.com>
2023-03-08 13:26:56 +0800

    updateee

--- 09_time_keeping/clock_v2/src/main.rs
+++ 09_time_keeping/clock_v2/src/main.rs
@@ -4,6 +4,7 @@ use libc;
 use chrono::{DateTime, Duration as ChronoDuration, TimeZone, Timelike};
 use chrono::{Local, Utc};
 
+use byteorder::{BigEndian, ReadBytesExt};
 use clap::{App, Arg, ArgMatches};
 
 // Standard Libraries
@@ -74,12 +75,45 @@ impl From<DateTime<Utc>> for NTPTimestamp {
 
         NTPTimestamp { 
             seconds: secs as u32, 
-            fraction: fraction as u32 }
+            fraction: fraction as u32 
+        }
+    }
+}
+
+impl NTPMessage {
+    fn new() -> Self {
+        NTPMessage { 
+            data: [0; NTP_MESSAGE_LENGTH], 
+        }
+    }
+
+    fn client() -> Self {
+        const VERSION: u8 = 0b00_011_000;
+        const MODE: u8 = 0b00_000_011;
+
+        let mut msg = NTPMessage::new();
+
+        msg.data[0] |= VERSION;
+        msg.data[0] |= MODE;
+        msg
+    }
+
+    fn parse_timestamp(
+        &self,
+        i: usize,
+
+    ) -> Result<NTPTimestamp, std::io::Error> {
+        let mut reader = &self.data[i..i + 8];
+        let seconds = reader.read_u32::<BigEndian>()?;
+        let fraction = reader.read_u32::<BigEndian>()?;
+
+        Ok(NTPTimestamp { seconds: seconds, fraction: fraction })
     }
 }
 
 
 struct Clock;
+
 impl Clock {
     fn get() -> DateTime<Local> {
         Local::now()
