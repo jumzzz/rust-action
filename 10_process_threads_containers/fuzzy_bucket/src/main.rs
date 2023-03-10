23ec183e00c0305c29dc6e1b5bbd83dcb54441ee
jumzzz <jummel19@gmail.com>
2023-03-10 09:58:00 +0800

    another fuzzy stuff

--- 10_process_threads_containers/fuzzy_bucket/src/main.rs
+++ 10_process_threads_containers/fuzzy_bucket/src/main.rs
@@ -1,10 +1,19 @@
 /// Understanding ranges i0..i1
 fn main() {
+    println!("x..=y range syntax");
     let v = vec![1,2,3,4,5,6,7,8,9,10];
     for v0 in &v {
         match v0 {
             1..=5 => println!("first half = {}", v0),
             6..=10 => println!("second half = {}", v0),
+            _ =>  println!("anything = {}", v0),
+        };
+    }
+    println!("bitwise_or |");
+    for v0 in &v {
+        match v0 {
+            1 | 2 | 3 | 4 | 5 => println!("first half = {}", v0),
+            6 | 7 | 8 | 9 | 10 => println!("second half = {}", v0),
             _ =>  println!("anything = {}", v0),
         };
     }
--- 10_process_threads_containers/render-hex/src/main.rs
+++ 10_process_threads_containers/render-hex/src/main.rs
@@ -126,6 +126,13 @@ fn parse(input: &str) -> Vec<Operation> {
     let mut steps = Vec::<Operation>::new();
     for byte in input.bytes() {
         let step = match byte {
+            b'0' => Home,
+            b'1'..=b'9' => {
+                let distance = (byte - 0x30) as isize;
+                Forward(distance * (HEIGHT / 10))
+            }
+            b'a' | b'b' | b'c' => TurnLeft,
+            b'd' | b'e' | b'f' => TurnRight,
             _ => Noop(byte),
         };
     }
