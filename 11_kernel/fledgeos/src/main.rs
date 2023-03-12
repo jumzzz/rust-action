140402edc3f340ff753d73c4c191bce216f175c4
jumzzz <jummel19@gmail.com>
2023-03-12 22:16:32 +0800

    comments

--- 11_kernel/fledgeos/src/main.rs
+++ 11_kernel/fledgeos/src/main.rs
@@ -1,11 +1,27 @@
 #![no_std]
 #![no_main]
 #![feature(core_intrinsics)]  // Unlocks LLVM Internals
+#![feature(lang_items)]
 
 use core::intrinsics;
 use core::panic::PanicInfo;
 
 use x86_64::instructions::{hlt};
+
+#[allow(unused)]              // We won't use all the enum values in our code
+#[derive(Clone, Copy)]        // Opt to Copy semantics
+#[repr(u8)]                   // Instruct the compiler to use single bytes for each enums
+enum Color {
+  Black = 0x0,      White = 0xF,
+  Blue = 0x1,       BrightBlue = 0x9,
+  Green = 0x2,      BrightGreen = 0xA,
+  Cyan = 0x3,       BrightCyan = 0xB,
+  Red = 0x4,        BrightRed = 0xC,
+  Magenta = 0x5,    BrightMagenta = 0xD,
+  Brown = 0x6,      Yellow = 0xE,
+  Gray = 0x7,       DarkGray = 0x8
+}
+
 
 #[panic_handler]              // Flag for panic handling
 #[no_mangle]                  // Prevents symbol naming 
@@ -14,6 +30,13 @@ pub fn panic(_info: &PanicInfo) -> ! {
     intrinsics::abort(); 
   }
 }
+
+/// This supposedly handles exception handling. Thus, the "eh"
+///  in "eh_personality"
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn eh_personality() { }
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
